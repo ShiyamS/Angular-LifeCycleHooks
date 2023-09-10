20e001a0a3e324aa7f001335f076dd8a5c67909e
Shiyam <shiyams315@gmail.com>
2023-09-10 19:36:04 +0530

    How Constructed is initiated first, and the userInput is logged but not reflected in the DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LifeCycleHooks';
+
+  userEnteredValue: string = ''
+
+  submitName(inputValue: HTMLInputElement) {
+    this.userEnteredValue = inputValue.value;
+  }
 }
